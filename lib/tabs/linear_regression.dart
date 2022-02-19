a89545d5b40e98436953af1f2f74ace7ad615f8d
Robin Gauthey <gautheyrobin@gmail.com>
2022-02-19 15:03:21 +0100

    added button style

--- lib/tabs/linear_regression.dart
+++ lib/tabs/linear_regression.dart
@@ -16,43 +16,79 @@ class linear_regression extends StatelessWidget {
         const SizedBox(
           height: 30,
         ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: const [
-            Text("Valeur 1 : "),
-            SizedBox(
-              width: 150,
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'X',
-                ),
-              ),
-            ),
-            SizedBox(
-              width: 150,
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Y',
-                ),
-              ),
-            )
-          ],
-        ),
+        const Point(),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const SizedBox(height: 100),
-            ElevatedButton(
-              style: style,
-              onPressed: () {},
-              child: const Text('Ajouter un point'),
+            SizedBox(
+              height: 50.0,
+              child: RaisedButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0),
+                    side: const BorderSide(
+                        color: Color.fromRGBO(0, 160, 227, 1))),
+                onPressed: () {},
+                padding: const EdgeInsets.all(20.0),
+                color: Colors.white,
+                textColor: const Color.fromRGBO(0, 160, 227, 1),
+                child: const Text("Ajouter un point",
+                    style: TextStyle(fontSize: 15)),
+              ),
+            ),
+            const SizedBox(width: 40),
+            SizedBox(
+              height: 50.0,
+              child: RaisedButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0),
+                    side: const BorderSide(
+                        color: Color.fromRGBO(220, 20, 60, 1))),
+                onPressed: () {},
+                padding: const EdgeInsets.all(20.0),
+                color: Colors.white,
+                textColor: const Color.fromRGBO(220, 20, 60, 1),
+                child: const Text("Supprimer le dernier point",
+                    style: TextStyle(fontSize: 15)),
+              ),
             ),
           ],
-
         ),
       ],
     ));
+  }
+}
+
+class Point extends StatelessWidget {
+  const Point({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: const [
+        Text("Valeur 1 : "),
+        SizedBox(
+          width: 150,
+          child: TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'X',
+            ),
+          ),
+        ),
+        SizedBox(
+          width: 150,
+          child: TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Y',
+            ),
+          ),
+        )
+      ],
+    );
   }
 }
