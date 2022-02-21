5ca6a418a8fb1681802424d2dca4514429b2fe7c
Robin Gauthey <gautheyrobin@gmail.com>
2022-02-21 16:42:23 +0100

    tabbarview scrollable

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class HomePage extends StatelessWidget {
       length: 2,
       child: Scaffold(
         appBar: AppBar(
-          bottom: const TabBar(
+          bottom: TabBar(
             tabs: [
               Tab(icon: Icon(Icons.scatter_plot), text: "Régression Linéaire"),
               Tab(
@@ -47,90 +47,99 @@ class HomePage extends StatelessWidget {
   }
 }
 
-
 //////////////////////////// REGRESSION LINEAIRE ////////////////////////////
 
-
 class regressionLineaire extends StatefulWidget {
-
   @override
   State createState() => _MyState();
 }
 
 class _MyState extends State<regressionLineaire> {
+  void changeColor() {
+    setState(() {
+      //TODO: Ajouter Textfields
+      //TODO: Ajouter infiniteScroll
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 30,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: const [
-                Text("Point 1 : ", style:TextStyle(
-                  fontWeight: FontWeight.w500,
-                )),
-                SizedBox(
-                  width: 150,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'X',
+    return Scaffold(
+      body: Center(
+          child: ListView(
+              shrinkWrap: false,
+              children: <Widget>[
+                Column(
+                  children: [
+                    const SizedBox(
+                      height: 30,
                     ),
-                  ),
-                ),
-                SizedBox(
-                  width: 150,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Y',
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: const [
+                        Text("Point 1 : ", style:TextStyle(
+                          fontWeight: FontWeight.w500,
+                        )),
+                        SizedBox(
+                          width: 115,
+                          child: TextField(
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'X',
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 115,
+                          child: TextField(
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Y',
+                            ),
+                          ),
+                        )
+                      ],
                     ),
-                  ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const SizedBox(height: 100),
+                        SizedBox(
+                          height: 50.0,
+                          child: RaisedButton(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18.0),
+                                side: const BorderSide(
+                                    color: Color.fromRGBO(0, 160, 227, 1))),
+                            onPressed: () {},
+                            padding: const EdgeInsets.all(15.0),
+                            color: Colors.white,
+                            textColor: const Color.fromRGBO(0, 160, 227, 1),
+                            child: const Text("Ajouter un point",
+                                style: TextStyle(fontSize: 15)),
+                          ),
+                        ),
+                        const SizedBox(width: 10),
+                        SizedBox(
+                          height: 50.0,
+                          child: RaisedButton(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18.0),
+                                side: const BorderSide(
+                                    color: Color.fromRGBO(220, 20, 60, 1))),
+                            onPressed: () {},
+                            padding: const EdgeInsets.all(15.0),
+                            color: Colors.white,
+                            textColor: const Color.fromRGBO(220, 20, 60, 1),
+                            child: const Text("Supprimer le dernier point",
+                                style: TextStyle(fontSize: 15)),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 )
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(height: 100),
-                SizedBox(
-                  height: 50.0,
-                  child: RaisedButton(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(18.0),
-                        side: const BorderSide(
-                            color: Color.fromRGBO(0, 160, 227, 1))),
-                    onPressed: () {},
-                    padding: const EdgeInsets.all(15.0),
-                    color: Colors.white,
-                    textColor: const Color.fromRGBO(0, 160, 227, 1),
-                    child: const Text("Ajouter un point",
-                        style: TextStyle(fontSize: 15)),
-                  ),
-                ),
-                const SizedBox(width: 10),
-                SizedBox(
-                  height: 50.0,
-                  child: RaisedButton(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(18.0),
-                        side: const BorderSide(
-                            color: Color.fromRGBO(220, 20, 60, 1))),
-                    onPressed: () {},
-                    padding: const EdgeInsets.all(15.0),
-                    color: Colors.white,
-                    textColor: const Color.fromRGBO(220, 20, 60, 1),
-                    child: const Text("Supprimer le dernier point",
-                        style: TextStyle(fontSize: 15)),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ));
+              ])),
+    );
   }
 }
