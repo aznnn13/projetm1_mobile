41c03be05b83529fa3cb263def2aa9eb8a4d92ae
Robin Gauthey <gautheyrobin@gmail.com>
2022-02-21 15:04:08 +0100

    stateful regression widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,175 +1,136 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'tabs/linear_regression.dart';
-import 'tabs/formulaire.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        debugShowCheckedModeBanner: false, title: "ProjetM1", home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: const TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.scatter_plot), text: "Régression Linéaire"),
+              Tab(
+                icon: FaIcon(FontAwesomeIcons.listOl),
+                text: "Formulaire",
+              ),
+            ],
+          ),
+          title: const Align(
+            alignment: Alignment.center,
+            child: Text(
+              "Projet M1",
+            ),
+          ),
+        ),
+        body: TabBarView(children: [
+          Text("Test"),
+          Text("Test"),
+        ]),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+//////////////////////////// REGRESSION LINEAIRE ////////////////////////////
 
-  final String title;
+
+class regressionLineaire extends StatefulWidget {
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State createState() => _MyState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _MyState extends State<regressionLineaire> {
+  Color _containerColor = Colors.yellow;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return MaterialApp(
-      home: DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            bottom: const TabBar(
-              tabs: [
-                Tab(
-                    icon: Icon(Icons.scatter_plot),
-                    text: "Régression Linéaire"),
-                Tab(
-                  icon: FaIcon(FontAwesomeIcons.listOl),
-                  text: "Formulaire",
+    return Center(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: const [
+                Text("Point 1 : "),
+                SizedBox(
+                  width: 150,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'X',
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 150,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Y',
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 100),
+                SizedBox(
+                  height: 50.0,
+                  child: RaisedButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),
+                        side: const BorderSide(
+                            color: Color.fromRGBO(0, 160, 227, 1))),
+                    onPressed: () {},
+                    padding: const EdgeInsets.all(15.0),
+                    color: Colors.white,
+                    textColor: const Color.fromRGBO(0, 160, 227, 1),
+                    child: const Text("Ajouter un point",
+                        style: TextStyle(fontSize: 15)),
+                  ),
+                ),
+                const SizedBox(width: 10),
+                SizedBox(
+                  height: 50.0,
+                  child: RaisedButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),
+                        side: const BorderSide(
+                            color: Color.fromRGBO(220, 20, 60, 1))),
+                    onPressed: () {},
+                    padding: const EdgeInsets.all(15.0),
+                    color: Colors.white,
+                    textColor: const Color.fromRGBO(220, 20, 60, 1),
+                    child: const Text("Supprimer le dernier point",
+                        style: TextStyle(fontSize: 15)),
+                  ),
                 ),
               ],
             ),
-            title: const Align(
-              alignment: Alignment.center,
-              child: Text(
-                "Projet M1",
-              ),
-            ),
-          ),
-          body: TabBarView(children: [
-            Center(
-                child: Column(
-                  children: [
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: const [
-                        Text("Point 1 : "),
-                        SizedBox(
-                          width: 150,
-                          child: TextField(
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: 'X',
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-                          width: 150,
-                          child: TextField(
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: 'Y',
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const SizedBox(height: 100),
-                        SizedBox(
-                          height: 50.0,
-                          child: RaisedButton(
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(18.0),
-                                side: const BorderSide(
-                                    color: Color.fromRGBO(0, 160, 227, 1))),
-                            onPressed: () {},
-                            padding: const EdgeInsets.all(15.0),
-                            color: Colors.white,
-                            textColor: const Color.fromRGBO(0, 160, 227, 1),
-                            child: const Text("Ajouter un point",
-                                style: TextStyle(fontSize: 15)),
-                          ),
-                        ),
-                        const SizedBox(width: 10),
-                        SizedBox(
-                          height: 50.0,
-                          child: RaisedButton(
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(18.0),
-                                side: const BorderSide(
-                                    color: Color.fromRGBO(220, 20, 60, 1))),
-                            onPressed: () {},
-                            padding: const EdgeInsets.all(15.0),
-                            color: Colors.white,
-                            textColor: const Color.fromRGBO(220, 20, 60, 1),
-                            child: const Text("Supprimer le dernier point",
-                                style: TextStyle(fontSize: 15)),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                )),
-            Text("Formulaire"),
-          ]),
-        ),
-      ),
-    );
+          ],
+        ));
   }
 }
-
